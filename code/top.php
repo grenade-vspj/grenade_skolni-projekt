a26d48732d0340898f4b2834699bff4e47a93eea
Fery007 <73137324+Fery007@users.noreply.github.com>
2020-11-29 13:03:54 +0100

    úprava menu

---
+++ code/top.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#"><img src="logo.jpg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Časopis<span class="sr-only">(current)</span></a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Aktuality</a>
+      </li>	  
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Příští čísla</a>
+      </li>	 	  
+      <li class="nav-item">
+        <a class="nav-link" href="#">VŠPJ</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Sponzoři</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Administrace</a>
+      </li>
+      
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit"> Search </button>
+    </form>
+      <a href="conn.php"><button class="btn btn-info" type="submit">Login</button></a>
+  </div>
+</nav>
