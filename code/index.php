4cae124b6279c09ed87a3f176ab7ab5b0f7f0d00
Lukáš Trnka <trnka08@student.vspj.cz>
2020-12-31 13:50:04 +0100

    Oprava formátování úvodní strany

--- code/index.php
+++ code/index.php
@@ -49,12 +49,10 @@ if (substr($url, -1) !== '/') {
 
   <div class="starter-template">
     <h1>LOGOS POLYTECHNIKOS</h1>
-    
   </div>
-    <h2 class="mt-4">Vydané články</h2>
   
-  <div class="row mb-3">
-      <p><strong>LOGOS POLYTECHNIKOS je vysokoškolský odborný recenzovaný časopis</strong>, který slouží pro publikační aktivity akademických pracovníků Vysoké školy polytechnické Jihlava i jiných vysokých škol, univerzit a výzkumných organizací. Je veden na seznamu recenzovaných odborných a vědeckých časopisů <strong>ERIH PLUS - </strong>European Reference Index for the Humanities and the Social Sciences (https://dbh.nsd.uib.no/publiseringskanaler/erihplus/periodical/info?id=488187).</p>
+  <div class="mb-3">
+      <p><strong>LOGOS POLYTECHNIKOS je vysokoškolský odborný recenzovaný časopis</strong>, který slouží pro publikační aktivity akademických pracovníků Vysoké školy polytechnické Jihlava i jiných vysokých škol, univerzit a výzkumných organizací. Je veden na seznamu recenzovaných odborných a vědeckých časopisů <strong>ERIH&nbsp;PLUS - </strong>European Reference Index for the Humanities and the Social Sciences (https://dbh.nsd.uib.no/publiseringskanaler/erihplus/periodical/info?id=488187).</p>
       <p>Od roku 2010 do roku 2018 byl časopis vydáván čtyřikrát ročně v elektronické a tištěné podobě. Od roku 2019 vychází třikrát ročně v elektronické verzi. Redakční rada časopisu sestává z&nbsp;interních i externích odborníků. Funkci šéfredaktora zastává prorektor pro tvůrčí a projektovou činnost Vysoké školy polytechnické Jihlava. Funkce odpovědných redaktorů jednotlivých čísel přísluší vedoucím kateder Vysoké školy polytechnické Jihlava. Veškeré vydávané příspěvky prochází recenzním řízením a jsou pečlivě redigovány.&nbsp;</p>
       <p><strong>Tematické a obsahové zaměření časopisu</strong> reflektuje potřeby oborových kateder Vysoké školy polytechnické Jihlava. Na základě souhlasu odpovědného redaktora mohou katedry poskytnout publikační prostor i&nbsp;odborníkům bez zaměstnanecké vazby k Vysoké škole polytechnické Jihlava.</p>
       <p><strong>V časopise je možné publikovat</strong> odborné články, statě, přehledové studie, recenze a další typy odborných příspěvků v&nbsp;českém, slovenském a anglickém jazyce. Do recenzního řízení jsou přijímány příspěvky tematicky odpovídající zaměření časopisu a formálně upravené dle redakční šablony (viz níže).<br /><br /></p>
@@ -73,10 +71,12 @@ if (substr($url, -1) !== '/') {
       <p>Pokud rozsah doručených příspěvků překročí kapacitu příslušného vydání, bude přijímání příspěvků ukončeno před uvedeným termínem.</p>
       <p>Adresa pro odesílání příspěvků:&nbsp;<em>logos&#x40;vspj.cz <br /></em>Kontaktní osoba: Bc. Zuzana Mafková: <a href="mai&#x6C;&#x74;&#x6F;&#x3A;zuzana.mafkova&#x40;vspj.cz">zuzana.mafkova&#x40;vspj.cz</a></p>
       <p>Adresa nakladatele: Vysoká škola polytechnická Jihlava, redakce LOGOS POLYTECHNIKOS, Tolstého 1556/16, 586 01 Jihlava<br /><br /><br /><strong>ISSN 2464-7551 (Online)</strong><br /><br /><em>Registrace MK ČR E 19390 – pro čísla z let 2010 až 2018 (vydávání tištěné verze časopisu bylo ukončeno). </em><br /><em>ISSN 1804-3682 (Print)&nbsp;– pro čísla z let 2010 až 2018 (vydávání tištěné verze časopisu bylo ukončeno).</em></p>
-      <b>Šéfredaktor</b>
-      doc. Ing. Zdeněk Horák, Ph.D. (Vysoká škola polytechnická Jihlava)<br><br>
-      <b>Redakční rada</b><br />
-      prof. PhDr. RNDr. Martin Boltižiar, PhD. (Univerzita Konštantína Filozofa v&nbsp;Nitre)<br />prof. RNDr. Helena Brožová, CSc. (Česká zemědělská univerzita v&nbsp;Praze)<br />doc. PhDr. Lada Cetlová, PhD. (Vysoká škola polytechnická Jihlava)<br />prof. Mgr. Ing. Martin Dlouhý, Dr. MSc. (Vysoká škola ekonomická v&nbsp;Praze)<br />prof. Ing. Tomáš Dostál, DrSc. (Vysoká škola polytechnická Jihlava)<br />doc. Ing. Jiří Dušek, Ph.D. (Vysoká škola evropských a regionálních studií)<br />doc. RNDr. Petr Gurka, CSc. (Vysoká škola polytechnická Jihlava)<br />Ing. Veronika Hedija, Ph.D. (Vysoká škola polytechnická Jihlava)<br />doc. Ing. Zdeněk Horák, Ph.D. (Vysoká škola polytechnická Jihlava)<br />Ing. Ivica Linderová, PhD. (Vysoká škola polytechnická Jihlava)<br />prof. MUDr. Aleš Roztočil, CSc. (Vysoká škola polytechnická Jihlava)<br />doc. PhDr. David Urban, Ph.D. (Vysoká škola polytechnická Jihlava)<br />doc. Dr. Ing. Jan Voráček, CSc. (Vysoká škola polytechnická Jihlava)<br />RNDr. PaedDr. Ján Veselovský, PhD. (Univerzita Konštantína Filozofa v&nbsp;Nitre)<br />doc. Ing. Libor Žídek, Ph.D. (Masarykova univerzita Brno)</p>
+      <p><b>Šéfredaktor</b><br/>
+          doc. Ing. Zdeněk Horák, Ph.D. (Vysoká škola polytechnická Jihlava)
+      </p>
+      <p><b>Redakční rada</b><br/>
+          prof. PhDr. RNDr. Martin Boltižiar, PhD. (Univerzita Konštantína Filozofa v&nbsp;Nitre)<br />prof. RNDr. Helena Brožová, CSc. (Česká zemědělská univerzita v&nbsp;Praze)<br />doc. PhDr. Lada Cetlová, PhD. (Vysoká škola polytechnická Jihlava)<br />prof. Mgr. Ing. Martin Dlouhý, Dr. MSc. (Vysoká škola ekonomická v&nbsp;Praze)<br />prof. Ing. Tomáš Dostál, DrSc. (Vysoká škola polytechnická Jihlava)<br />doc. Ing. Jiří Dušek, Ph.D. (Vysoká škola evropských a regionálních studií)<br />doc. RNDr. Petr Gurka, CSc. (Vysoká škola polytechnická Jihlava)<br />Ing. Veronika Hedija, Ph.D. (Vysoká škola polytechnická Jihlava)<br />doc. Ing. Zdeněk Horák, Ph.D. (Vysoká škola polytechnická Jihlava)<br />Ing. Ivica Linderová, PhD. (Vysoká škola polytechnická Jihlava)<br />prof. MUDr. Aleš Roztočil, CSc. (Vysoká škola polytechnická Jihlava)<br />doc. PhDr. David Urban, Ph.D. (Vysoká škola polytechnická Jihlava)<br />doc. Dr. Ing. Jan Voráček, CSc. (Vysoká škola polytechnická Jihlava)<br />RNDr. PaedDr. Ján Veselovský, PhD. (Univerzita Konštantína Filozofa v&nbsp;Nitre)<br />doc. Ing. Libor Žídek, Ph.D. (Masarykova univerzita Brno)
+      </p>
       <br/>
       
           
